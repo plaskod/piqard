37873932f4fb834af9f91f82df54248a114d7229
Mateusz <mateusz.politycki@gmail.com>
2022-11-08 00:45:48 +0100

    Prepare Prompt class for loading yaml config files

--- config.py
+++ config.py
@@ -1,9 +1,7 @@
 from information_retrieval.google_custom_search.gcs import GoogleCustomSearch
 from large_language_models.bloom_176b_api.bloom_176b_api import BLOOM176bAPI
 from large_language_models.gpt_j6b_api.gpt_j6b_api import GPTj6bAPI
-from prompting.basic_prompt import BasicPrompt
-from prompting.best_truncated_prompt import BestTruncatedPrompt
-from prompting.few_shot_prompt import FewShowPrompt
+from prompting.prompt import Prompt
 
 result_dir = "result"
 
@@ -11,4 +9,4 @@ result_dir = "result"
 class PIQARDConfig:
     information_retriever = GoogleCustomSearch()
     large_language_model = BLOOM176bAPI()
-    prompt_generator = FewShowPrompt()
+    prompt_generator = Prompt("prompting/templates/5_shot_prompt.yaml")
--- prompting/prompt.py
+++ prompting/prompt.py
@@ -1,13 +1,16 @@
 from typing import Optional
+import yaml
 
 
 class Prompt:
-    def __init__(self, prompt: str):
-        self.prompt = prompt
+    def __init__(self, prompt_path: str):
+        with open(prompt_path, 'r') as file:
+            template = yaml.safe_load(file)
+        self.prompt = template['text']
+        self.info = template['info']
 
-    def generate(self, question: str, documents: Optional[list[str]]) -> str:
-        pass
+    def generate(self, question: str, context: Optional[str]) -> str:
+        return self.prompt.format(question=question, context=context if context else '')
 
     def __str__(self) -> str:
-        pass
-    
+        return self.info
