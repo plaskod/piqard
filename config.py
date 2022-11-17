61e45add39db43428b4af5b67de9c290a2043faf
Mateusz <mateusz.politycki@gmail.com>
2022-11-17 12:04:54 +0100

    Change prompt templates extension to JINJA

--- config.py
+++ config.py
@@ -5,8 +5,6 @@ from information_retrieval.vector_retriever.vector_retirever import VectorRetrie
 from information_retrieval.google_custom_search.gcs import GoogleCustomSearch
 from large_language_models.bloom_176b_api.bloom_176b_api import BLOOM176bAPI
 from large_language_models.gpt_j6b_api.gpt_j6b_api import GPTj6bAPI
-from prompting.prompt import Prompt
-
 
 
 result_dir = "result"
@@ -15,5 +13,5 @@ result_dir = "result"
 class PIQARDConfig:
     information_retriever = None
     large_language_model = BLOOM176bAPI()
-    prompt_generator = Prompt("prompting/templates/5_shot_openbookqa.yaml")
+    prompt_template = "prompting/templates/context_prompt.txt"
     context_builder = TruncatedContextBuilder()
