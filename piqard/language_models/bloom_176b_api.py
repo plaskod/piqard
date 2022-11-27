834280671b03afcbc3f7bcd4a0c229ef95ad7d17
Mateusz <mateusz.politycki@gmail.com>
2022-11-27 13:26:43 +0100

    Add n shot test openbookqa

--- piqard/language_models/bloom_176b_api.py
+++ piqard/language_models/bloom_176b_api.py
@@ -10,16 +10,17 @@ class BLOOM176bAPI(LanguageModel):
             with open(
                 "assets/credentials/huggingface.json", "r"
             ) as f:
-                API_KEY = json.load(f)["APIkey"]
+                self.API_KEY = json.load(f)["APIkey"]
         except (FileNotFoundError, KeyError):
             print("api_key.json not found or incorrect file structure.")
             exit(0)
         self.API_URL = "https://api-inference.huggingface.co/models/bigscience/bloom"
-        self.headers = {"Authorization": f"Bearer {API_KEY}"}
         self.options = {"temperature": 1.0, "do_sample": False}
 
     def query(self, payload: str) -> str:
-        response = requests.post(self.API_URL, headers=self.headers, json={'inputs': payload, "parameters": self.options})
+        response = requests.post(self.API_URL,
+                                 headers={"Authorization": f"Bearer {self.API_KEY}"},
+                                 json={'inputs': payload, "parameters": self.options})
         data = response.json()
         if type(data) == dict and 'error' in data.keys():
             raise LanguageModelAPIOverloadException
